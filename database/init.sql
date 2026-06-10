0cdd4b4d39071dd439db74472913d5cc04a92a38
ahsan <ahsan@local>
2026-06-10 19:14:03 +0700

    feat: seed data for locations, users, staff assignments

--- database/init.sql
+++ database/init.sql
@@ -135,4 +135,41 @@ CREATE TABLE public.fcm_tokens (
 
 -- Seed data: locations
 INSERT INTO public.locations (id, name, type, parent_id, created_at, updated_at) VALUES
-('aaeb4ceb-9bde-46cc-8cf3-c0331b82e621', 'Gedung', 'UNIVERSITAS', NULL, now(), now());
+('1061b345-33c0-461a-b6c7-b995440b6d29', 'UIN Sultan Maulana Hasanuddin Banten', 'UNIVERSITAS', NULL, now(), now()),
+('d9c17ff0-8f43-460e-903c-006e1154d54a', 'Sains dan Teknologi', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('a34cdced-df20-4887-aeae-589e5029944d', 'Adab dan Humaniora', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('43a03e07-e0d9-4cef-96c3-6700c130a7cb', 'Ushuluddin', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('d1395635-434d-4623-afcb-c357d7d9c0ad', 'Dakwah', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('63abc627-51a8-4e05-ad82-bf9e69b12545', 'Tarbiyah dan Keguruan', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('a8573e9f-8d2c-4bcd-9e6d-ed28fed9474a', 'Syariah', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('7494b96b-0b16-4409-9e75-2f401ce5b611', 'Psikologi', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('4f500cd0-8fc1-4464-8fe6-173c9ae8bbb7', 'Hukum', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('cf8e6f05-fa7f-4a64-9451-34637462ea80', 'Ekonomi dan Bisnis Islam', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now()),
+('d78c22ab-cca8-44dc-bb41-854dfdb42617', 'Farmasi', 'FAKULTAS', '1061b345-33c0-461a-b6c7-b995440b6d29', now(), now());
+
+-- Seed data: users
+INSERT INTO public.users (id, name, "NIM", password, role, created_at) VALUES
+('b83e1f32-2a1d-4696-b14d-bff26cf461e6', 'Admin', 'admin', '$2b$10$uWbdHDIsBg7GdAT4uIjbEeUgcDl7HjGdkHd8SsqaRt9CVSjGbviwq', 'ADMIN', now()),
+('abae62ef-929e-4e43-8ecc-9f6755363cc2', 'Staff Saintek', 'staff-saintek', '$2b$10$ph1ijjOAV1KfgdCL2pSwd.DXrow8G4lUVdpcJp0sM7OrpHG3jLCj.', 'STAFF', now()),
+('0fd4a5d8-2668-4b9f-8f8f-ca0fa636d9b9', 'Staff Adhum', 'staff-adhum', '$2b$10$u1SNsHR0l.L1hCqUZmdX8u3oWa2YNa0WcS8/EFDf2vzjx/k.QRRwW', 'STAFF', now()),
+('93f6ba1c-8af3-4ea2-90a3-e71b11719dcc', 'Staff Ushuluddin', 'staff-ushuluddin', '$2b$10$uo9qK593aD754aDCBqzFyevqmfuRrhRZOkdphSGzN/5RMjmjv1HFu', 'STAFF', now()),
+('1314ffe0-f3c2-4a1c-9e40-b581f1feeb2f', 'Staff Dakwah', 'staff-dakwah', '$2b$10$NDhVpwZNjcWkqNu3MIeVPOVKfXnZDpV1c1QnCKdSBR9kIcwkFV5Wq', 'STAFF', now()),
+('9bbe8a0a-c931-446a-93a4-9ab71c803cfa', 'Staff Tarbiyah', 'staff-tarbiyah', '$2b$10$EOHjDuwumOBbIuy5SlHh7ezVGsPpU5KhxcbB2UyX9eTHzU.AOdAtK', 'STAFF', now()),
+('336b53a7-26f8-4600-9a81-dd49a30984f4', 'Staff Syariah', 'staff-syariah', '$2b$10$/FVST1sFySYbDR9JQBQxQuDKrokuL.MEKYQ6IBcdIPnxh7WC.0Tby', 'STAFF', now()),
+('08b12ff3-67a7-45fc-a6d1-b88baf87b988', 'Staff Psikologi', 'staff-psikologi', '$2b$10$Xadi2Vw0deNgUKzV.4NkTelHKyO.Vo/Nf.rvdLC52/qp7w/eMp2SC', 'STAFF', now()),
+('807114c5-08e2-4148-ba8d-2b49254f6ff6', 'Staff Hukum', 'staff-hukum', '$2b$10$omRffdq0K2ByxRypEkD0vee22hHjwY.WpSaeKolHllUm6FAt4wAYK', 'STAFF', now()),
+('c831313b-997b-4f52-bcea-9a7f9ba6bf72', 'Staff Ekonomi', 'staff-ekonomi', '$2b$10$UBMCGE3WnSS0yumKwHLJZOQpZp1H2WnmBhfg/Dfo2mGa5Ig34wO5y', 'STAFF', now()),
+('1c8c2193-984f-41d8-bc90-d715f79daad8', 'Staff Farmasi', 'staff-farmasi', '$2b$10$bwizRrYK4VwjuuJ7uhSyMuTeWrjpOdnlDZJ3RCh1WLRiLdLu8Kx96', 'STAFF', now());
+
+-- Seed data: user_staff_location
+INSERT INTO public.user_staff_location (staff_id, location_id, created_at) VALUES
+('abae62ef-929e-4e43-8ecc-9f6755363cc2', 'd9c17ff0-8f43-460e-903c-006e1154d54a', now()),
+('0fd4a5d8-2668-4b9f-8f8f-ca0fa636d9b9', 'a34cdced-df20-4887-aeae-589e5029944d', now()),
+('93f6ba1c-8af3-4ea2-90a3-e71b11719dcc', '43a03e07-e0d9-4cef-96c3-6700c130a7cb', now()),
+('1314ffe0-f3c2-4a1c-9e40-b581f1feeb2f', 'd1395635-434d-4623-afcb-c357d7d9c0ad', now()),
+('9bbe8a0a-c931-446a-93a4-9ab71c803cfa', '63abc627-51a8-4e05-ad82-bf9e69b12545', now()),
+('336b53a7-26f8-4600-9a81-dd49a30984f4', 'a8573e9f-8d2c-4bcd-9e6d-ed28fed9474a', now()),
+('08b12ff3-67a7-45fc-a6d1-b88baf87b988', '7494b96b-0b16-4409-9e75-2f401ce5b611', now()),
+('807114c5-08e2-4148-ba8d-2b49254f6ff6', '4f500cd0-8fc1-4464-8fe6-173c9ae8bbb7', now()),
+('c831313b-997b-4f52-bcea-9a7f9ba6bf72', 'cf8e6f05-fa7f-4a64-9451-34637462ea80', now()),
+('1c8c2193-984f-41d8-bc90-d715f79daad8', 'd78c22ab-cca8-44dc-bb41-854dfdb42617', now());
